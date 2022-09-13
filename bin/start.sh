4ded724c64e725e2c0cefe8c44824fe7f9e22978
Aaron Mason <absorbentshoulderman@gmail.com>
2022-09-13 16:14:47 +1000

    start script
    Start work on starting services

--- bin/start.sh
+++ bin/start.sh
@@ -2,12 +2,15 @@
 
 SSHD_KEY_LOC=/etc/ssh/keys/
 
+addgroup git
+addgroup www-data
+
 for SSHD_USER in gituser gitreader
 do
     if ! getent passwd ${SSHD_USER} > /dev/null
     then
-        useradd --shell /usr/bin/git-shell --create-home ${SSHD_USER}
-        install --owner ${SSHD_USER} --group ${SSHD_USER} --mode 700 --directory /home/${SSHD_USER}/.ssh
+        adduser -s /usr/bin/git-shell -D -G git ${SSHD_USER}
+        install --owner ${SSHD_USER} --group git --mode 700 --directory /home/${SSHD_USER}/.ssh
         if [ -f "${SSHD_KEY_LOC}/${SSHD_USER}" ]
         then
             install --owner ${SSHD_USER} --group ${SSHD_USER} --mode 700 "${SSHD_KEY_LOC}/${SSHD_USER}" /home/${SSHD_USER}/.ssh/authorized_keys
@@ -37,9 +40,19 @@ then
     install --directory /var/run/sshd
 fi
 
-exec $(which sshd) \
-    -D \
-    -e \
-    -p ${SSHD_PORT}
+# fcgiwrap for gitweb
+FCGIWRAP_USER=fcgiwrap
+FCGIWRAP_GRP=www-data
+FCGIWRAP_SOCK=/run/fcgiwrap/fcgiwrap.sock
+adduser -s /bin/false -S -D -h /var/run/fcgiwrap -G $FCGIWRAP_GRP $FCGIWRAP_USER
+[ -d /run/fcgiwrap ] || mkdir -p /run/fcgiwrap
+chown ${FCGIWRAP_USER}:${FCGIWRAP_GRP} /run/fcgiwrap
+exec su -p $FCGIWRAP_USER -c $(which fcgiwrap)' -c '$(nproc)' -s unix:'${FCGIWRAP_SOCK}
+chmod g+w ${FCGIWRAP_SOCK}
 
-# TODO: Run gitweb and nginx
+# nginx
+sed -i 's/\$SERVER_NAME/'$(hostname -f)'/g' /etc/nginx/http.d/default.conf
+exec nginx -c /etc/nginx/nginx.conf
+
+# ssh - which will keep this thing alive
+exec $(which sshd) -D -e -p ${SSHD_PORT}
