5142b556d066b2b80a407e4c59a21579989c079c
Aaron Mason <absorbentshoulderman@gmail.com>
2022-09-28 16:01:12 +1000

    start.sh
    - Add SSL capability for nginx
    - Run SSHd as a daemon
    - Process every sixty seconds to add new/remove old ssh auth keys (which also keeps the docker alive)

--- bin/start.sh
+++ bin/start.sh
@@ -13,9 +13,10 @@ do
         install --owner ${SSHD_USER} --group git --mode 700 --directory /home/${SSHD_USER}/.ssh
         if [ -f "${SSHD_KEY_LOC}/${SSHD_USER}" ]
         then
-            install --owner ${SSHD_USER} --group ${SSHD_USER} --mode 700 "${SSHD_KEY_LOC}/${SSHD_USER}" /home/${SSHD_USER}/.ssh/authorized_keys
+            install --owner ${SSHD_USER} --group git --mode 700 "${SSHD_KEY_LOC}/${SSHD_USER}" /home/${SSHD_USER}/.ssh/authorized_keys
         else
-            echo "NOTE: user ${SSHD_USER} has no authorized_keys file - access will not be possible"
+            echo "NOTE: user ${SSHD_USER} has no authorized_keys file - access will not be possible. Empty file created."
+            touch "${SSHD_KEY_LOC}/${SSHD_USER}"
         fi
     fi
 done
@@ -51,8 +52,26 @@ exec su -p $FCGIWRAP_USER -c $(which fcgiwrap)' -c '$(nproc)' -s unix:'${FCGIWRA
 chmod g+w ${FCGIWRAP_SOCK}
 
 # nginx
-sed -i 's/\$SERVER_NAME/'$(hostname -f)'/g' /etc/nginx/http.d/default.conf
-exec nginx -c /etc/nginx/nginx.conf
+if [ -f /opt/config/httpd-default.conf ]
+then
+    cp /opt/config/httpd-default.conf /etc/nginx/http.d/default.conf
+else
+    sed -i 's/\$SERVER_NAME/'$(hostname -f)'/g' /etc/nginx/http.d/default.conf
+    cp /etc/nginx/http.d/default.conf /opt/config/httpd-default.conf
+fi
+[ -f /opt/config/httpd-ssl.conf ] && cp /opt/config/httpd-ssl.conf /etc/nginx/http.d/ssl.conf
+[ -f /opt/config/ssl.crt ] && install -o root -u root -m 0644 /opt/config/ssl.crt /etc/ssl/certs/httpd.crt
+[ -f /opt/config/ssl.key ] && install -o root -u root -m 0640 /opt/config/ssl.key /etc/ssl/private/httpd.key
+exec nginx -c /etc/nginx/nginx.conf || exit 1
 
 # ssh - which will keep this thing alive
-exec $(which sshd) -D -e -p ${SSHD_PORT}
+exec $(which sshd) -p ${SSHD_PORT} || exit 2
+
+# SSH authorised keys update
+while sleep 60
+do
+    for SSHD_USER in gituser gitreader
+    do
+        install --owner ${SSHD_USER} --group git --mode 700 "${SSHD_KEY_LOC}/${SSHD_USER}" /home/${SSHD_USER}/.ssh/authorized_keys
+    done
+done
