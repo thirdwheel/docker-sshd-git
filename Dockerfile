e729e06f6e193c9caf3bfe19c1706f222051bc1e
Aaron Mason <aaron.mason@horts.com.au>
2022-10-25 09:42:38 +1100

    Make this thing work!

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,20 @@
 FROM alpine:3.14
 
 ENV SSHD_PORT=22
+ENV HTTP_PORT=80
+ENV HTTPS_PORT=443
 
-RUN apk update \
- && apk add --no-cache \
-    openssh-server \
-    git \
-    git-gitweb \
-    nginx \
-    fcgiwrap \
- && sed --in-place --regexp-extended \
+RUN apk update
+RUN apk add --no-cache openssh-server git git-gitweb nginx fcgiwrap sudo perl-cgi
+RUN sed --in-place --regexp-extended \
     --expression 's|^#(PasswordAuthentication\s+).*|\1no|' \
     --expression 's|^#(GatewayPorts\s+).*|\1yes|' \
-    /etc/ssh/sshd_config \
- && rm --recursive --force /tmp/* /var/tmp/*
+    /etc/ssh/sshd_config
+RUN rm -R -f /tmp/* /var/tmp/*
+RUN [ -f /etc/ssh/ssh_host_ecdsa_key ] || ssh-keygen -q -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa
+RUN [ -f /etc/ssh/ssh_host_rsa_key ] || ssh-keygen -q -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+RUN [ -f /etc/ssh/ssh_host_dsa_key ] || ssh-keygen -q -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+RUN [ -f /etc/ssh/ssh_host_ed25519_key ] || ssh-keygen -q -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519
 
 COPY bin/start.sh /opt/start.sh
 COPY etc/httpd-default.conf /etc/nginx/http.d/default.conf
@@ -22,5 +23,7 @@ COPY etc/gitweb.conf /etc/gitweb.conf
 VOLUME [ "/opt/config", "/opt/git/public", "/opt/git/private" ]
 
 EXPOSE ${SSHD_PORT}
+EXPOSE ${HTTP_PORT}
+EXPOSE ${HTTPS_PORT}
 
 CMD ["/opt/start.sh"]
--- bin/start.sh
+++ bin/start.sh
@@ -1,39 +1,39 @@
-#!/bin/bash
+#!/bin/sh
 
-SSHD_KEY_LOC=/etc/ssh/keys/
+SSHD_KEY_LOC=/config/ssh/keys
+
+if [ ! -d $SSHD_KEY_LOC ]
+then
+    install --directory $SSHD_KEY_LOC
+fi
 
 addgroup git
-addgroup www-data
 
 for SSHD_USER in gituser gitreader
 do
     if ! getent passwd ${SSHD_USER} > /dev/null
     then
         adduser -s /usr/bin/git-shell -D -G git ${SSHD_USER}
-        install --owner ${SSHD_USER} --group git --mode 700 --directory /home/${SSHD_USER}/.ssh
+        install --owner ${SSHD_USER} --group git --mode 700 --directory "/home/${SSHD_USER}/.ssh"
         if [ -f "${SSHD_KEY_LOC}/${SSHD_USER}" ]
         then
-            install --owner ${SSHD_USER} --group git --mode 700 "${SSHD_KEY_LOC}/${SSHD_USER}" /home/${SSHD_USER}/.ssh/authorized_keys
+            install --owner ${SSHD_USER} --group git --mode 700 "${SSHD_KEY_LOC}/${SSHD_USER}" "/home/${SSHD_USER}/.ssh/authorized_keys"
         else
             echo "NOTE: user ${SSHD_USER} has no authorized_keys file - access will not be possible. Empty file created."
-            touch "${SSHD_KEY_LOC}/${SSHD_USER}"
+            touch "${SSHD_KEY_LOC}/${SSHD_USER}" "/home/${SSHD_USER}/.ssh/authorized_keys"
         fi
     fi
 done
 
-if [ ! -d /config/ssh/keys ]
-then
-    install --directory /config/ssh/keys
-fi
-
 for HOST_KEY in /etc/ssh/ssh_host_*_key
 do
-    TYPE=$(cut -d_ -f3 <<< ${HOST_KEY##*/})
-    if [ ! -f /config/ssh/keys/ssh_host_${TYPE}_key ]; then
-        ssh-keygen -q -f /config/ssh/keys/ssh_host_${TYPE}_key -t ${TYPE} -N ''
+    TYPE=$(echo ${HOST_KEY##*/} | cut -d_ -f3)
+    # If not already in the config partition, copy them there
+    if [ ! -f $SSHD_KEY_LOC/ssh_host_${TYPE}_key ]; then
+        cp /etc/ssh/ssh_host_${TYPE}_key* $SSHD_KEY_LOC
     fi
-    rm --force /etc/ssh/ssh_host_${TYPE}_key*
-    ln --symbolic /config/ssh/keys/ssh_host_${TYPE}_key* /etc/ssh
+    rm -f /etc/ssh/ssh_host_${TYPE}_key*
+    ln -s $SSHD_KEY_LOC/ssh_host_${TYPE}_key* /etc/ssh
 done
 
 if [ ! -d /var/run/sshd ]
@@ -41,14 +41,18 @@ then
     install --directory /var/run/sshd
 fi
 
+[ ! -d /run/fcgiwrap ] || install --directory --owner fcgiwrap --group www-data /run/fcgiwrap
+
 # fcgiwrap for gitweb
 FCGIWRAP_USER=fcgiwrap
 FCGIWRAP_GRP=www-data
 FCGIWRAP_SOCK=/run/fcgiwrap/fcgiwrap.sock
-adduser -s /bin/false -S -D -h /var/run/fcgiwrap -G $FCGIWRAP_GRP $FCGIWRAP_USER
 [ -d /run/fcgiwrap ] || mkdir -p /run/fcgiwrap
 chown ${FCGIWRAP_USER}:${FCGIWRAP_GRP} /run/fcgiwrap
-exec su -p $FCGIWRAP_USER -c $(which fcgiwrap)' -c '$(nproc)' -s unix:'${FCGIWRAP_SOCK}
+echo Starting fcgiwrap...
+sudo -u $FCGIWRAP_USER $(which fcgiwrap) -c $(nproc) -s unix:${FCGIWRAP_SOCK} &
+sleep 5
+[ -S ${FCGIWRAP_SOCK} ] || exit 3
 chmod g+w ${FCGIWRAP_SOCK}
 
 # nginx
@@ -60,14 +64,17 @@ else
     cp /etc/nginx/http.d/default.conf /opt/config/httpd-default.conf
 fi
 [ -f /opt/config/httpd-ssl.conf ] && cp /opt/config/httpd-ssl.conf /etc/nginx/http.d/ssl.conf
-[ -f /opt/config/ssl.crt ] && install -o root -u root -m 0644 /opt/config/ssl.crt /etc/ssl/certs/httpd.crt
-[ -f /opt/config/ssl.key ] && install -o root -u root -m 0640 /opt/config/ssl.key /etc/ssl/private/httpd.key
-exec nginx -c /etc/nginx/nginx.conf || exit 1
+[ -f /opt/config/ssl.crt ] && install -o root -g root -m 0644 /opt/config/ssl.crt /etc/ssl/certs/httpd.crt
+[ -f /opt/config/ssl.key ] && install -o root -g root -m 0640 /opt/config/ssl.key /etc/ssl/private/httpd.key
+echo Starting nginx...
+nginx -c /etc/nginx/nginx.conf || exit 1
 
-# ssh - which will keep this thing alive
-exec $(which sshd) -p ${SSHD_PORT} || exit 2
+# sshd
+echo Starting sshd...
+/usr/sbin/sshd -p ${SSHD_PORT} || exit 2
 
 # SSH authorised keys update
+echo Starting ssh authorised keys update...
 while sleep 60
 do
     for SSHD_USER in gituser gitreader
